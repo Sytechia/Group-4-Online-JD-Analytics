5124613de2cd889945056b710ae72ec4aaa9def2
Zachary Phoon <73163517+S3rade@users.noreply.github.com>
2024-09-18 00:05:12 +0800

    Fixed the skill image loading bug 2
    image 1 was not loading in time

--- run.py
+++ run.py
@@ -3,12 +3,14 @@ from scrapy.crawler import CrawlerProcess
 from blueprints import app
 import os
 from controllers.crawler import JobSpider
+from controllers.skill_diagram import soft_skill, hard_skills
 
 if __name__ == "__main__":
-    process = CrawlerProcess()
-    process.crawl(JobSpider)
-    process.start()
-
+    #process = CrawlerProcess()
+    #process.crawl(JobSpider)
+    #process.start()
+    soft_skill()
+    hard_skills()
     app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'uploads')
     app.run(debug=True, threaded=True)
     
