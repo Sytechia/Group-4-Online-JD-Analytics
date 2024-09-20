3bbee08626644dccc34ef553bda256e338bc7e7d
huntereureka <53546873+huntereureka@users.noreply.github.com>
2024-09-20 13:24:58 +0800

    Delete .venv directory

--- .venv/Lib/site-packages/pdfreader/filters/dct.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-filter_names = ('DCTDecode', 'DCT')
-
-
-def decode(binary, *_):
-    img = Image.frombytes(binary)
-    return img.tobytes()
--- .venv/Lib/site-packages/pdfreader/filters/jpx.py
+++
@@ -1,9 +0,0 @@
-import logging
-log = logging.getLogger(__name__)
-
-filter_names = ('JPXDecode',)
-
-
-def decode(binary, params):
-    log.warning("JPX decoder is not implemented. It returns raw unfiltered data.")
-    return binary
