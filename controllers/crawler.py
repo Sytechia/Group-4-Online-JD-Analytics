f9825ee11ee7f75d670cdcc0acdaadf22d0d51c2
ray <ray.wyn1102@gmail.com>
2024-09-24 11:40:23 +0800

    Updated crawler.py w/o routing

--- controllers/crawler.py
+++ controllers/crawler.py
@@ -3,7 +3,6 @@ import scrapy
 import sqlite3
 from scrapy.crawler import CrawlerProcess
 
-
 from blueprints import setup_database
 from controllers.parse_data import parse_job
 
@@ -12,25 +11,44 @@ import requests
 
 class JobSpider(scrapy.Spider):
     name = "job_spider"
-    start_urls = [
-        "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Python&location=Singapore&geoId=102454443&trk=public_jobs_jobs-search-bar_search-submit&start=0"
-    ]
-
-    def __init__(self):
+    def __init__(self, keyword=None, start=0, *args, **kwargs):
+        super(JobSpider, self).__init__(*args, **kwargs)
+        self.keyword = keyword
+        self.start = int(start)
+        self.start_urls = [
+            f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={self.keyword}&location=Singapore&geoId=102454443&trk=public_jobs_jobs-search-bar_search-submit&start={self.start}"
+        ]
         print("Initializing JobSpider and getting db connection!")
-        self.conn = sqlite3.connect('../database.db')  # Establish a connection to the database
-        self.cursor = self.conn.cursor()  # Initialize the cursor
-        self.parsed_jobs = []
+        try:
+            self.conn = sqlite3.connect('../database.db')  # Establish a connection to the database
+            self.cursor = self.conn.cursor()  # Initialize the cursor
+            self.parsed_jobs = []
+
+        except sqlite3.Error as e:
+            self.logger.error(f"Error connecting to database: {e}")
 
     def parse(self, response):
         yield scrapy.Request(url=self.start_urls[0], callback=self.parse_data)
 
     #calls the data cleaning function
     def parse_data(self, response):
-        # yield from parse_job(response, self.cursor, self.conn)
-        for job_item in parse_job(response, self.cursor, self.conn):
-            self.parsed_jobs.append(job_item)
+        try:
+            jobs = parse_job(response, self.cursor, self.conn)
+            for job_item in jobs:
+                self.parsed_jobs.append(job_item)
+            if len(jobs) == 25: 
+                self.start += 25
+                next_page = f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={self.keyword}&location=Singapore&geoId=102454443&trk=public_jobs_jobs-search-bar_search-submit&start={self.start}"
+                yield scrapy.Request(url=next_page, callback=self.parse_data)
 
+        except Exception as e:
+            self.logger.error(f"Error parsing data: {e}")
+
+    # close the database session
+    def close(self, reason):
+        if self.conn:
+            self.conn.close()
+            self.logger.info("Database connection closed.")
 
 if __name__ == "__main__":
     conn, cursor = setup_database()
