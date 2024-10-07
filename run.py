7314c71a3aefe9cc59d679301751b11ee19e242d
ray <ray.wyn1102@gmail.com>
2024-10-07 11:56:37 +0800

    Updated run.py

--- run.py
+++ run.py
@@ -2,8 +2,26 @@
 
 from blueprints import app
 import os
+import threading
 from controllers.crawler import JobSpider
+from scrapy.crawler import CrawlerProcess
+from twisted.internet import defer, task
 
-if __name__ == "__main__":  
+def start_crawler():
+    process = CrawlerProcess()
+    process.crawl(JobSpider)
+
+    @defer.inlineCallbacks
+    def crawl():
+        yield process.crawl(JobSpider)
+
+    def run_crawler():
+        task.LoopingCall(crawl).start(1800.0)   # 30-minute interval
+
+    process.start(run_crawler())
+
+if __name__ == "__main__":
     app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'uploads')
-    app.run( debug=True,threaded=True)
+    crawler_thread = threading.Thread(target=start_crawler)
+    crawler_thread.start()
+    app.run(debug=True, threaded=True)
